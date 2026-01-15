caf042ed66686b027e18fc1fb6454c45a8541f31
Ujjwal-Aggrawal <ujjmee2279@gmail.com>
2026-01-16 01:19:45 +0530

    feat(app): scrap tolerance percentage to be applied on production entry level

--- ujwal_industries/hooks.py
+++ ujwal_industries/hooks.py
@@ -137,13 +137,11 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+	"Stock Entry": {
+		"validate": "ujwal_industries.ujwal_industries.overrides.stock_entry.validate_scrap_item_tolerance"
+	}
+}
 
 # Scheduled Tasks
 # ---------------
