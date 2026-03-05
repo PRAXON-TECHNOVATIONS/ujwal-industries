b585d2bdd6da5ae0492f88548a024dc116220b7e
Ujjwal-Aggrawal <ujjmee2279@gmail.com>
2026-03-05 13:42:47 +0530

    feat(BUY001): PO register and rejection report has beeb added

---
+++ ujwal_industries/ujwal_industries/report/po_register/po_register.js
@@ -0,0 +1,63 @@
+frappe.query_reports["PO Register"] = {
+	filters: [
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			reqd: 1,
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+		},
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+		},
+		{
+			fieldname: "supplier",
+			label: __("Supplier"),
+			fieldtype: "Link",
+			options: "Supplier",
+		},
+		{
+			fieldname: "item_code",
+			label: __("Item Code"),
+			fieldtype: "Link",
+			options: "Item",
+		},
+		{
+			fieldname: "item_group",
+			label: __("Item Group"),
+			fieldtype: "Link",
+			options: "Item Group",
+		},
+		{
+			fieldname: "warehouse",
+			label: __("Warehouse"),
+			fieldtype: "Link",
+			options: "Warehouse",
+		},
+		{
+			fieldname: "status",
+			label: __("PO Status"),
+			fieldtype: "Select",
+			options: "\nDraft\nTo Receive and Bill\nTo Bill\nTo Receive\nCompleted\nCancelled\nClosed",
+		},
+	],
+
+	formatter(value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (column.fieldname === "pending_qty" && data && data.pending_qty > 0) {
+			value = `<span style="color: #d97706; font-weight: 600;">${value}</span>`;
+		}
+		return value;
+	},
+};
