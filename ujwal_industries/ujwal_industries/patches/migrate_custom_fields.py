5dd554005dbb3da79dbe0ed0720a0e07039936ee
Ujjwal-Aggrawal <ujjmee2279@gmail.com>
2026-03-16 22:48:09 +0530

    fix(BulkProd160326): fixed the old patches

--- ujwal_industries/ujwal_industries/patches/migrate_custom_fields.py
+++ ujwal_industries/ujwal_industries/patches/migrate_custom_fields.py
@@ -1,10 +1,10 @@
 # Copyright (c) 2026, Ujwal Industries and contributors
 # For license information, please see license.txt
 
-from .custom_fields import serial_no, manufacturing_settings
+from .custom_fields import manufacturing_setting, serial_no
 
 
 def run_all():
     """Run all custom field migrations"""
     serial_no.create_fields()
-    manufacturing_settings.create_fields()
+    manufacturing_setting.create_fields()
