ffd818e5056d26cb58e955d541ff2c5c4c8fc725
Anju1207 <sudeshkakran999@gmail.com>
2026-03-22 15:45:56 +0530

    Parallel manage date

--- ujwal_industries/hooks.py
+++ ujwal_industries/hooks.py
@@ -34,7 +34,7 @@ app_include_css = "/assets/ujwal_industries/css/custom_modal.css?V=0.1.29"
 app_include_js = [
 	"/assets/ujwal_industries/js/custom_dialog.js?V=0.1.29",
 	"/assets/ujwal_industries/js/manage_dates_dialog.js?V=0.1.29",
-	# "/assets/ujwal_industries/js/parallel_manage_dates_dialog.js?V=0.1.29",
+	"/assets/ujwal_industries/js/parallel_manage_dates_dialog.js?V=0.1.29",
 	# "/assets/ujwal_industries/js/grid_custom_icons.js",
 ]
 # include js, css files in header of web template
