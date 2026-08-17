91af6f41f9b17a36b9170c28c5a33d421185967b
Smuggler0p <avimathur789@gmail.com>
2026-08-17 13:21:50 +0000

    feat: added Other costs and profit calculations in Cost Estimation

--- ujwal_industries/ujwal_industries/doctype/cost_estimation/cost_estimation.js
+++ ujwal_industries/ujwal_industries/doctype/cost_estimation/cost_estimation.js
@@ -5,10 +5,28 @@ frappe.ui.form.on("Cost Estimation", {
 	refresh: function (frm) {
 		calculate_rm_totals(frm);
 	},
+	inventory_carrying_pct: calculate_other_costs,
+	packing_forwarding_pct: calculate_other_costs,
+	rejection_pct: calculate_other_costs,
+	profit_mode: calculate_totals,
+	profit_pct: calculate_totals,
+	profit_amount: calculate_totals,
 });
 
 frappe.ui.form.on("Cost Estimation RM Item", {
-	rm_used: calculate_rm_row,
+	rm_used: function (frm, cdt, cdn) {
+		const row = locals[cdt][cdn];
+		if (!row.rm_used) {
+			return;
+		}
+		frappe.db.get_value("Item", row.rm_used, "last_purchase_rate").then(({ message }) => {
+			if (message.last_purchase_rate) {
+				row.rm_rate_per_kg = message.last_purchase_rate;
+			}
+			frm.refresh_field("rm_items");
+			calculate_rm_row(frm, cdt, cdn);
+		});
+	},
 	rm_rate_per_kg: calculate_rm_row,
 	gross_wt_per_pc: calculate_rm_row,
 	rm_items_remove: function (frm) {
@@ -28,9 +46,18 @@ frappe.ui.form.on("Cost Estimation Scrap Item", {
 frappe.ui.form.on("Cost Estimation Operation Item", {
 	time_per_pc_min: calculate_operation_row,
 	shift_rate_per_min: calculate_operation_row,
+	rate_per_pc: function (frm, cdt, cdn) {
+		const row = locals[cdt][cdn];
+		if (!row.workstation) {
+			// no machine — rate_per_pc is typed directly, just re-sum totals
+			calculate_totals(frm);
+		}
+	},
 	workstation: function (frm, cdt, cdn) {
 		const row = locals[cdt][cdn];
 		if (!row.workstation) {
+			frm.refresh_field("operation_items");
+			calculate_totals(frm);
 			return;
 		}
 		frappe.db.get_value(
@@ -86,25 +113,63 @@ function calculate_rm_totals(frm) {
 	frm.refresh_field("total_scrap_price_per_pc");
 	frm.refresh_field("net_rm_cost_per_pc");
 
-	calculate_totals(frm);
+	calculate_other_costs(frm);
 }
 
 function calculate_operation_row(frm, cdt, cdn) {
 	const row = locals[cdt][cdn];
-	row.rate_per_pc = Math.ceil(flt(row.time_per_pc_min) * flt(row.shift_rate_per_min) * 100) / 100;
+	if (row.workstation) {
+		row.rate_per_pc = Math.ceil((flt(row.shift_rate_per_min) / flt(row.time_per_pc_min)) * 100) / 100;
+	}
 	frm.refresh_field("operation_items");
+	calculate_other_costs(frm);
+}
+
+function calculate_other_costs(frm) {
+	const base_for_pct = flt(frm.doc.net_rm_cost_per_pc) + get_total_labour_cost(frm);
+
+	let rm_value_for_inventory = 0;
+	(frm.doc.rm_items || []).forEach((row) => {
+		rm_value_for_inventory += flt(row.gross_wt_per_pc) * flt(row.rm_rate_per_kg);
+	});
+
+	frm.doc.inventory_carrying_cost = (rm_value_for_inventory * flt(frm.doc.inventory_carrying_pct)) / 100;
+	frm.doc.packing_forwarding_cost = (base_for_pct * flt(frm.doc.packing_forwarding_pct)) / 100;
+	frm.doc.rejection_cost = (base_for_pct * flt(frm.doc.rejection_pct)) / 100;
+
+	frm.refresh_field("inventory_carrying_cost");
+	frm.refresh_field("packing_forwarding_cost");
+	frm.refresh_field("rejection_cost");
+
 	calculate_totals(frm);
 }
 
-function calculate_totals(frm) {
+function get_total_labour_cost(frm) {
 	let total_labour_cost = 0;
 	(frm.doc.operation_items || []).forEach((row) => {
 		total_labour_cost += flt(row.rate_per_pc);
 	});
-
 	frm.doc.total_labour_cost_per_pc = total_labour_cost;
-	frm.doc.total_cost_per_pc = flt(frm.doc.net_rm_cost_per_pc) + total_labour_cost;
-
 	frm.refresh_field("total_labour_cost_per_pc");
+	return total_labour_cost;
+}
+
+function calculate_totals(frm) {
+	const total_labour_cost = get_total_labour_cost(frm);
+	const base_for_pct = flt(frm.doc.net_rm_cost_per_pc) + total_labour_cost;
+
+	frm.doc.total_cost_per_pc =
+		base_for_pct +
+		flt(frm.doc.inventory_carrying_cost) +
+		flt(frm.doc.packing_forwarding_cost) +
+		flt(frm.doc.rejection_cost);
 	frm.refresh_field("total_cost_per_pc");
+
+	if (frm.doc.profit_mode === "Percentage") {
+		frm.doc.profit_amount = (base_for_pct * flt(frm.doc.profit_pct)) / 100;
+		frm.refresh_field("profit_amount");
+	}
+
+	frm.doc.total_component_cost = flt(frm.doc.total_cost_per_pc) + flt(frm.doc.profit_amount);
+	frm.refresh_field("total_component_cost");
 }
